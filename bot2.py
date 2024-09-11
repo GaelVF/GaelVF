b7e5bb84318c5cbbd673761b4760ad8d273918e7
GaelVF <gaelvargasf593@gmail.com>
2024-09-11 18:37:56 -0500

    Bot2.py

--- bot2.py
+++ bot2.py
@@ -5,6 +5,9 @@ intents = discord.Intents.default()
 intents.message_content = True
 # Crear una instancia del bot con un prefijo
 bot = commands.Bot(command_prefix='$', intents=intents)
+import os
+print(os.listdir('Imagenes'))
+list_memes = os.listdir('Imagenes')
 
 # Diccionario con consejos para reducir residuos
 consejos = [
@@ -27,7 +30,7 @@ async def consejo(ctx):
     await ctx.send(random.choice(consejos))
 
 # Comando para clasificar residuos
-@bot.command(name='clasificar')
+@bot.command(name='clasificacion')
 async def clasificar(ctx, *, objeto):
     objeto = objeto.lower()
     categoria = clasificacion_residuos.get(objeto, "No tenemos información sobre este objeto.")
@@ -43,5 +46,31 @@ async def reto(ctx):
 async def on_ready():
     print(f"{bot.user.name} está listo y conectado.")
 
+@bot.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {bot.user}')
+
+
+@bot.command(name='ima')
+async def ima(ctx, animal: str):
+    imagenes = {
+        "loro": "Loro.jpeg",
+        "mapache": "Mapache.jpeg",
+        "oveja": "Oveja.jpeg",
+    }
+    imagen = imagenes.get(animal.lower(), None)
+    if imagen:
+        with open(f'Imagenes/{imagen}', 'rb') as f:
+            picture = discord.File(f)
+        await ctx.send(file=picture)
+    else:
+        await ctx.send("No tengo imagen para ese animal.")
+
+@bot.command(name='imagenaleatoria')
+async def imagenaleatoria(ctx):
+    select_ima = random.choice(list_memes)
+    with open(f'Imagenes/{select_ima}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
 # Ejecutar el bot
 bot.run("")
